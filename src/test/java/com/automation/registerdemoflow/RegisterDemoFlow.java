057bf1fef818578cc22af94bd3d6933e5452f309
Vinoth Rathinam <44257903+vinothrwins@users.noreply.github.com>
2025-02-19 15:47:52 +0530

    Update RegisterDemoFlow.java

--- src/test/java/com/automation/registerdemoflow/RegisterDemoFlow.java
+++ src/test/java/com/automation/registerdemoflow/RegisterDemoFlow.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 // import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-
+// Code added by Sandeep 
 public class RegisterDemoFlow {
 
 	public WebDriver driver;
